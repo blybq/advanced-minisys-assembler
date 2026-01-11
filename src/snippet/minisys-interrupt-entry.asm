81d316ad984667cc1a6087de9eab34549cc340d6
blybq <blybqljz@gmail.com>
2026-01-11 11:02:12 +0800

    修改了中断处理程序

--- src/snippet/minisys-interrupt-entry.asm
+++ src/snippet/minisys-interrupt-entry.asm
@@ -2,12 +2,25 @@
     push $t0
     push $t1
     push $t2
+    push $v0
+    push $s0
+
     mfc0 $t0, $13, 0        # $t0 <- CP0 Cause
     andi $t1, $t0, 0x007C   # keep only ExcCode[4:0] at Cause 2-6
+
+    lui $s0, 65535
+    ori $s0, $s0, 64608
+    addi $v0, $zero, 0x00FF
+    
+
     addi $t2, $zero, 0x0020 # Cause 2-6 of syscall is 01000
     beq $t1, $t2, _int_handler_syscall
     nop
+
+    sw $v0, 0($s0)
     # 如果不是syscall异常，恢复寄存器并返回
+    pop $s0
+    pop $v0
     pop $t2
     pop $t1
     pop $t0
--- src/snippet/minisys-interrupt-handler.asm
+++ src/snippet/minisys-interrupt-handler.asm
@@ -2,10 +2,13 @@ _int_handler_syscall:
   # handler of syscall
   # light last 4 leds
   addi $t0, $zero, 0x000F
-  addi $t1, $zero, 0xFC60
+  lui $t1, 65535
+  ori $t1, $t1, 64608        # LED地址
   sw $t0, 0($t1)
   nop
   nop
+  pop Ss0
+  pop $v0
   pop $t2
   pop $t1
   pop $t0
