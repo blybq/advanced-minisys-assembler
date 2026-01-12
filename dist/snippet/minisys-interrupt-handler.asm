3225db449b6f90923971890c6820ba075856e323
blybq <blybqljz@gmail.com>
2026-01-12 08:03:58 +0800

    中断处理保护寄存器

--- dist/snippet/minisys-interrupt-handler.asm
+++ dist/snippet/minisys-interrupt-handler.asm
@@ -2,57 +2,117 @@
 
 interruptServer0:
   # 时钟中断处理程序（中断号0）
-  # 保存寄存器
-  push $t0
-  push $t1
-  push $t2
-  push $t3
-  push $a0
-  push $a1
-  push $ra
+	push $t0
+	push $t1
+	push $t2
+	push $t3
+	push $t4
+	push $t5
+	push $t6
+	push $t7
+	push $t8
+	push $t9
   
-  # TODO: 在这里添加时钟中断处理逻辑
-  # 示例：可以从全局变量读取值，进行处理，然后写回
-  
-  # 恢复寄存器
-  pop $ra
-  pop $a1
-  pop $a0
-  pop $t3
-  pop $t2
-  pop $t1
-  pop $t0
-  
-  # 返回
-  eret
+	lui	$t0, 0
+	ori	$t0, $t0, 61440
+	lw	$t1, a($0)
+	nop
+	nop
+	and	$t2, $t1, $t0
+	sw	$t2, 0($sp)
+	addiu	$t3, $zero, 1
+	add	$t4, $t1, $t3
+	sw	$t4, 4($sp)
+	addiu	$t5, $zero, 4095
+	lw	$t6, 4($sp)
+	nop
+	nop
+	and	$t7, $t6, $t5
+	sw	$t7, 4($sp)
+	lw	$t8, 0($sp)
+	nop
+	nop
+	nor	$t9, $t8, $t8
+	lw	$t0, 4($sp)
+	nop
+	nop
+	nor	$t0, $t0, $t0
+	and	$t0, $t9, $t0
+	nor	$t0, $t0, $t0
+	sw	$t0, a($0)
+	addiu	$sp, $sp, 8
+
+	pop $t9
+	pop $t8
+	pop $t7
+	pop $t6
+	pop $t5
+	pop $t4
+	pop $t3
+	pop $t2
+	pop $t1
+	pop $t0
+	eret
   nop
 
 interruptServer1:
   # 键盘中断处理程序（中断号1）
   # 保存寄存器
-  push $t0
-  push $t1
-  push $t2
-  push $t3
-  push $a0
-  push $a1
-  push $ra
+	push $t0
+	push $t1
+	push $t2
+	push $t3
+	push $t4
+	push $t5
+	push $t6
+	push $t7
+	push $t8
+	push $t9
+
+	addiu	$t0, $zero, 4095
+	lw	$t1, a($0)
+	nop
+	nop
+	and	$t2, $t1, $t0
+	sw	$t2, 0($sp)
+	lui	$t3, 65535
+	ori	$t3, $t3, 64528
+	lw	$t4, 0($t3)
+	nop
+	nop
+	sw	$t4, 4($sp)
+	addiu	$t5, $zero, 12
+	lw	$t6, 4($sp)
+	nop
+	nop
+	sllv	$t7, $t6, $t5
+	sw	$t7, 4($sp)
+	lw	$t8, 0($sp)
+	nop
+	nop
+	nor	$t9, $t8, $t8
+	lw	$t0, 4($sp)
+	nop
+	nop
+	nor	$t0, $t0, $t0
+	and	$t0, $t9, $t0
+	nor	$t0, $t0, $t0
+	sw	$t0, a($0)
+	addiu	$sp, $sp, 8
   
-  # TODO: 在这里添加键盘中断处理逻辑
-  # 示例：读取键盘寄存器值，进行处理，然后写回全局变量
-  
-  # 恢复寄存器
-  pop $ra
-  pop $a1
-  pop $a0
-  pop $t3
-  pop $t2
-  pop $t1
-  pop $t0
-  
-  # 返回
-  eret
+	pop $t9
+	pop $t8
+	pop $t7
+	pop $t6
+	pop $t5
+	pop $t4
+	pop $t3
+	pop $t2
+	pop $t1
+	pop $t0
+	eret
   nop
+
 
 _syscall:
   # syscall处理程序（中断号5，作为异常处理）
