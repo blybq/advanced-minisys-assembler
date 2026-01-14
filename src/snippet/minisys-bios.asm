2e814fd9f74573be8c1d1bc7c79726916576fc66
blybq <blybqljz@gmail.com>
2026-01-14 20:34:04 +0800

    修改了BIOS注释

--- src/snippet/minisys-bios.asm
+++ src/snippet/minisys-bios.asm
@@ -4,7 +4,7 @@
     addi $s1,$zero,0xFFFF
     addi $s7,$zero,0xFC60   # LED基址
     sw	 $s1,0($s7)         # LED全亮
-    # 数码管刷新SEU09172
+    # 数码管循环显示20260101
     # lui  $s5,8
     addi $s5,$zero,0x000A
     addi $s3,$zero,0xFC00   # 段码基址
